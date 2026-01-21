308c1deced0f79303c2b75e107ba0800737e666c
Paulo Cidro <paulocidro@gmail.com>
2026-01-21 11:53:01 -0300

    page not found criada

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,5 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/notfound.css";
+
 const NotFound = () => {
-  return <div></div>;
+  return (
+    <div className="not-found-container">
+      <h1 className="not-found-title">404</h1>
+      <p className="not-found-text">Página não encontrada</p>
+      <Link to="/" className="home-button">
+        Volte para a home
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
